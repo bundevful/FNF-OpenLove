2b182a2510bf287d11a092e1a6849f991b9a2d8d
Stilic <lateman-jpeg@outlook.fr>
2025-10-05 20:38:54 +0200

    fix crash

--- funkin/backend/discord.lua
+++ funkin/backend/discord.lua
@@ -1,4 +1,4 @@
-local DiscordActivity = require "lib.discord"
+local success, DiscordActivity = pcall(require, "lib.discord")
 
 ---@class Discord
 local Discord = {}
@@ -17,26 +17,38 @@ local _options = {
 }
 
 function Discord.init()
-	DiscordActivity.start(Discord.clientID, true, function()
-		DiscordActivity.setActivity(_options)
-	end)
+	if success then
+		DiscordActivity.start(Discord.clientID, true, function()
+			DiscordActivity.setActivity(_options)
+		end)
 
-	Logger.log("debug", "Discord Activity started")
-	Discord.isInitialized = true
+		Logger.log("debug", "Discord Activity started")
+		Discord.isInitialized = true
+	end
 end
 
-function Discord.shutdown() DiscordActivity.shutdown() end
+function Discord.shutdown()
+	if success then
+		DiscordActivity.shutdown()
+	end
+end
 
 function Discord.changePresence(options)
-	if not Discord.isInitialized then return end
-	_options = options or {}
-	_options.assets = options.assets or {}
-	_options.assets.large_image = options.assets.large_image or "icon"
-	_options.assets.large_text = options.assets.large_text or "FNF LÖVE"
+	if success then
+		if not Discord.isInitialized then return end
+		_options = options or {}
+		_options.assets = options.assets or {}
+		_options.assets.large_image = options.assets.large_image or "icon"
+		_options.assets.large_text = options.assets.large_text or "FNF LÖVE"
 
-	DiscordActivity.setActivity(_options)
+		DiscordActivity.setActivity(_options)
+	end
 end
 
-function Discord.update() DiscordActivity.update() end
+function Discord.update()
+	if success then
+		DiscordActivity.update()
+	end
+end
 
 return Discord
